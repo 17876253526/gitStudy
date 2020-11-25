702575bd63c5f4ed593089b4cbf59012f366c8cd
“王才铭 <928788578@qq.com>
2020-11-25 22:46:48 +0800

    删除tstt 3 5 6

--- src/main/java/com/wcm/stduy/test5.java
+++
@@ -1,4 +0,0 @@
-package com.wcm.stduy;
-
-public class test5 {
-}
--- src/main/java/com/wcm/stduy/test6.java
+++
@@ -1,7 +0,0 @@
-package com.wcm.stduy;
-
-public class test6 {
-    public static void main(String[] args) {
-        System.out.println("!@#");
-    }
-}
