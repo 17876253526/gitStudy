620aa9ac01a0376fcc446b6cd1af928e19325a7c
“王才铭 <928788578@qq.com>
2021-06-07 17:34:48 +0800

    test6文件添加11111

--- src/main/java/com/wcm/stduy/test.java
+++ src/main/java/com/wcm/stduy/test.java
@@ -1,7 +1,9 @@
 package com.wcm.stduy;
 
-public class test {
+public class test6 {
     public static void main(String[] args) {
 
+        System.out.println("!@#");
+        System.out.println("特首t");
     }
 }
