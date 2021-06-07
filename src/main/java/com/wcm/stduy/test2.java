7a2d77595e016fbc767db049d299f33129f89eae
“王才铭 <928788578@qq.com>
2021-06-07 17:20:00 +0800

    test1分支合并到master

--- src/main/java/com/wcm/stduy/test2.java
+++ src/main/java/com/wcm/stduy/test2.java
@@ -3,5 +3,7 @@ package com.wcm.stduy;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("$$$$");
+
+        System.out.println("我是test1,即将合并到master");
     }
 }
