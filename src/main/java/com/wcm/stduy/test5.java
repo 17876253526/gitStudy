c64a97fbe11cf6a66460d73ec6540a1f247c1d91
“王才铭 <928788578@qq.com>
2021-06-07 19:03:10 +0800

    完善message v6.0
     Conflicts:
    	src/main/java/com/wcm/stduy/test2.java

--- src/main/java/com/wcm/stduy/test5.java
+++ src/main/java/com/wcm/stduy/test5.java
@@ -3,5 +3,6 @@ package com.wcm.stduy;
 public class test5 {
     public static void main(String[] args) {
         System.out.println("123123123");
+        System.out.println("11111111111111");
     }
 }
